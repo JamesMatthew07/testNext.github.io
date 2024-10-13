8ff267f1dc0b737fa034c7a69587cb11efe3c6f6
JamesMatthew07 <llanosjamesmatthew30@gmail.com>
2024-10-13 23:17:42 +0800

    added

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {};
 module.exports = {
     output: 'export',
     // Optional: If your project is not at the root, set basePath
-    basePath: '/repository-name', // replace 'repository-name' with your repo name
+    basePath: 'testNext.github.io', // replace 'repository-name' with your repo name
     images: {
       unoptimized: true, // Disable Next.js Image optimization
     },
