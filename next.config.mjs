d4e4cb9ebaa05f0265121d46c557a928cf15abec
JamesMatthew07 <llanosjamesmatthew30@gmail.com>
2024-10-13 23:07:45 +0800

    Added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+module.exports = {
+    output: 'export',
+    // Optional: If your project is not at the root, set basePath
+    basePath: '/repository-name', // replace 'repository-name' with your repo name
+    images: {
+      unoptimized: true, // Disable Next.js Image optimization
+    },
+  };
+
 export default nextConfig;
