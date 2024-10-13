7ae095183159d0994d497b3a09f517ea46305fe1
JamesMatthew07 <llanosjamesmatthew30@gmail.com>
2024-10-13 23:21:20 +0800

    Done

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-module.exports = {
+
+// @ts-check
+ 
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
     output: 'export',
     // Optional: If your project is not at the root, set basePath
     basePath: 'testNext.github.io', // replace 'repository-name' with your repo name
     images: {
       unoptimized: true, // Disable Next.js Image optimization
     },
-  };
-
-export default nextConfig;
+  }
+   
+  export default nextConfig
